0b599b1eb6fefbf4d65f74e6ee80a281a44e364d
lady2110 <lady-2110@hotmail.com>
2021-09-20 20:34:11 -0500

    Se adicciona splash

---
+++ app/src/main/java/com/fenix/proyectofenix/splash.java
@@ -0,0 +1,60 @@
+package com.fenix.proyectofenix;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class splash extends AppCompatActivity {
+
+    // Declaración de atributos o variables
+    ProgressBar pb;
+    int contador=0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //Método para transición automatica entre activity
+
+        prog();
+
+        TimerTask entrada = new TimerTask() {
+            @Override
+            public void run() {
+                Intent cambio = new Intent(splash.this, MainActivity.class);
+                startActivity(cambio);
+                finish();
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(entrada,4000);
+    }
+
+    //Método para activar y visualizar el ProgressBar
+    public void prog()
+    {
+        pb= (ProgressBar) findViewById(R.id.barra);
+        final Timer ft = new Timer();
+        TimerTask tt = new TimerTask() {
+            @Override
+            public void run() {
+                contador++;
+                pb.setProgress(contador);
+                if(contador ==100)
+                    ft.cancel();
+            }
+        };
+        ft.schedule(tt,0,40);
+    }
+}
+
