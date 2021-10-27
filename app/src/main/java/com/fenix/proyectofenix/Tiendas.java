c78ca27f287f4fde9114771a4b646a8f34eddd2e
Leidy <lady-2110@hotmail.com>
2021-10-26 19:27:40 -0500

    crud tienda, interfaz add produstos, interfaz menu

--- app/src/main/java/com/fenix/proyectofenix/Tiendas.java
+++ app/src/main/java/com/fenix/proyectofenix/Tiendas.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Menu extends AppCompatActivity {
+public class Tiendas extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_tiendas);
     }
 }
---
+++ app/src/main/java/com/fenix/proyectofenix/model/Tienda.java
@@ -0,0 +1,2 @@
+package com.fenix.proyectofenix.model;public class Tienda {
+}
